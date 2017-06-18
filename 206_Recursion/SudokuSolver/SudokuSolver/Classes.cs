a27912533725e2c816f010c2c7cbcbe0ef24ba9a
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 16:55:20 +0200

    206_Recursion Sudoku Solver, Final commit

---
+++ 206_Recursion/SudokuSolver/SudokuSolver/Classes.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Sudoku
+    {
+        int[,] t;
+        Position[] fixPos, emptyPos;
+
+        public Sudoku(int[,] t)
+        {
+            this.t = t;
+            FieldSelection();
+        }
+
+        void FieldSelection()
+        {
+            // How many zero imtes we have
+            int db = 0;
+            for (int i = 0; i < t.GetLength(0); i++)
+                for (int j = 0; j < t.GetLength(1); j++)
+                    if (t[i, j] != 0)
+                        db++;
+
+            // Creating the blocks
+            fixPos = new Position[db];
+            emptyPos = new Position[(t.GetLength(0) * t.GetLength(1)) - db];
+
+            // Rechecking the matrix
+            int idxf = 0, idxu = 0;
+            for (int i = 0; i < t.GetLength(0); i++)
+                for (int j = 0; j < t.GetLength(1); j++)
+                    if (t[i, j] != 0)
+                        fixPos[idxf++] = new Position(i, j);
+                    else
+                        emptyPos[idxu++] = new Position(i, j);
+        }
+
+        void FieldConcatenation(int[] e)
+        {
+            for (int i = 0; i < e.Length; i++)
+                t[emptyPos[i].Row, emptyPos[i].Column] = e[i];
+        }
+
+        public bool Solution()
+        {
+            bool exists = false;
+            int[] e = new int[emptyPos.Length];
+            BackTrack(0, ref exists, e);
+            if (exists)
+                FieldConcatenation(e);
+            return exists;
+        }
+
+        bool ft(int level, int number)
+        {
+            int i = 0;
+            while (i < fixPos.Length && !(number == t[fixPos[i].Row, fixPos[i].Column] && Position.Block(emptyPos[level], fixPos[i])))
+                i++;
+            return i == fixPos.Length;
+        }
+        
+        bool fk(int level, int number, int k, int knumber)
+        {
+            return !(number == knumber && Position.Block(emptyPos[level], emptyPos[k]));
+        }
+
+        void BackTrack(int level, ref bool exists, int[] e)
+        {
+            int i = 0;
+            do
+            {
+                i++;
+                if(ft(level, i))
+                {
+                    int k = 0;
+                    while (k < level && fk(level, i, k, e[k]))
+                        k++;
+                    if(k == level)
+                    {
+                        e[level] = i;
+                        if (level == emptyPos.Length - 1)
+                            exists = true;
+                        else
+                            BackTrack(level + 1, ref exists, e);
+                    }
+                }
+            }
+            while (!exists && i < 9);
+        }
+    }
+
+    class Position
+    {
+        int column, row;
+        public int Column { get { return column; } }
+        public int Row { get { return row; } }
+
+        public Position(int column, int row)
+        {
+            this.column = column;
+            this.row = row;
+        }
+
+        public static bool Block(Position p1, Position p2)
+        {
+            return ((p1.Row == p2.Row) || (p1.Column == p2.Column) || ((p1.Row / 3 == p2.Row / 3) && (p1.Column / 3 == p2.Column / 3)));
+        }
+    }
+}
---
+++ 206_Recursion/SudokuSolver/SudokuSolver/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SudokuSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] example = new int[,]
+            {
+                {5,3,0,0,7,0,0,0,0},
+                {6,0,0,1,9,5,0,0,0},
+                {0,9,8,0,0,0,0,6,0},
+                {8,0,0,0,6,0,0,0,3},
+                {4,0,0,8,0,3,0,0,1},
+                {7,0,0,0,2,0,0,0,6},
+                {0,6,0,0,0,0,2,8,0},
+                {0,0,0,4,1,9,0,0,5},
+                {0,0,0,0,8,0,0,7,9},
+            };
+
+            WriteTable(example);
+
+            Sudoku s = new Sudoku(example);
+            if (s.Solution())
+                WriteTable(example);
+            else
+                Console.WriteLine("There is no solution!");
+
+            Console.ReadLine();
+        }
+
+        static void WriteTable(int[,] a)
+        {
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                    Console.Write(((a[i, j] == 0) ? "_" : a[i, j].ToString()) + " ");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+    }
+}

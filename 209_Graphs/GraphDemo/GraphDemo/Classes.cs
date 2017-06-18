e2579ce16acfa066c4ce322784192cfea54f9920
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 21:14:26 +0200

    209_Graphs Application, Final commit

---
+++ 209_Graphs/GraphDemo/GraphDemo/Classes.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDemo
+{
+    class GraphNeighbourList : Graph
+    {
+        List<int>[] L;
+
+        public GraphNeighbourList(int N)
+            :base (N)
+        {
+            L = new List<int>[N];
+            for (int i = 0; i < N; i++)
+                L[i] = new List<int>();
+        }
+
+        public override List<int> Peaks()
+        {
+            List<int> l = new List<int>();
+            for (int i = 0; i < N; i++)
+                l.Add(i);
+            return l;
+        }
+
+        public override void AddEdge(int from, int to)
+        {
+            L[from].Add(to);
+            L[to].Add(from);
+        }
+
+        public override bool LeadEdge(int from, int to)
+        {
+            return L[from].Contains(to);
+        }
+    }
+
+    class GraphPeakMatrix : Graph
+    {
+        int[,] CS;
+        
+        public GraphPeakMatrix(int N)
+            :base (N)
+        {
+            CS = new int[N, N];
+        }
+
+        public override List<int> Peaks()
+        {
+            List<int> l = new List<int>();
+            for (int i = 0; i < N; i++)
+                l.Add(i);
+            return l;
+        }
+
+        public override void AddEdge(int from, int to)
+        {
+            CS[from, to] = 1;
+            CS[to, from] = 1;
+        }
+
+        public override bool LeadEdge(int from, int to)
+        {
+            return CS[from, to] == 1;
+        }
+    }
+
+    abstract class Graph
+    {
+        protected int N;
+
+        public Graph(int N)
+        {
+            this.N = N;
+        }
+
+        public abstract List<int> Peaks();
+        public abstract void AddEdge(int from, int to);
+        public abstract bool LeadEdge(int from, int to);
+
+        public List<int> Neighbours(int peak)
+        {
+            List<int> l = new List<int>();
+            foreach (int i in Peaks())
+                if (LeadEdge(peak, i))
+                    l.Add(i);
+            return l;
+        }
+
+        public List<int> LattitudeRun()
+        {
+            List<int> F = new List<int>();
+            Queue<int> S = new Queue<int>();
+            F.Add(0);
+            S.Enqueue(0);
+            while(S.Count != 0)
+            {
+                int k = S.Dequeue();
+                foreach (int x in Neighbours(k))
+                {
+                    if(!F.Contains(x))
+                    {
+                        S.Enqueue(x);
+                        F.Add(x);
+                    }
+                }
+            }
+            return F;
+        }
+
+        public List<int> DepthRun()
+        {
+            List<int> F = new List<int>();
+            DepthRunRec(0, F);
+            return F;
+        }
+
+        void DepthRunRec(int k, List<int> F)
+        {
+            F.Add(k);
+            foreach (int x in Neighbours(k))
+                if (!F.Contains(x))
+                    DepthRunRec(x, F);
+        }
+    }
+}
---
+++ 209_Graphs/GraphDemo/GraphDemo/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GraphNeighbourList g1 = new GraphNeighbourList(7);
+            g1.AddEdge(0, 1);
+            g1.AddEdge(0, 3);
+            g1.AddEdge(0, 4);
+            g1.AddEdge(1, 2);
+            g1.AddEdge(1, 3);
+            g1.AddEdge(2, 3);
+            g1.AddEdge(2, 5);
+            g1.AddEdge(3, 5);
+            g1.AddEdge(5, 6);
+
+            Console.WriteLine("Csúcsok: ");
+            Write(g1.Peaks());
+
+            Console.WriteLine("0. csúcs szomszédai: ");
+            Write(g1.Neighbours(0));
+
+            Console.WriteLine("Szélességi bejárás: ");
+            Write(g1.LattitudeRun());
+
+            Console.WriteLine("Mélységi bejárás: ");
+            Write(g1.DepthRun());
+
+            Console.ReadLine();
+        }
+
+        static void Write(List<int> l)
+        {
+            foreach (int e in l)
+                Console.Write(e + " ");
+            Console.WriteLine();
+        }
+    }
+}

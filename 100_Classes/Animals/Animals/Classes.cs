f8b8e459d496f34d32d71d8021d2e8fb67250e55
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 12:30:41 +0200

    100_Classes Final commit

--- 100_Classes/Animals/Animals/Classes.cs
+++ 100_Classes/Animals/Animals/Classes.cs
@@ -6,13 +6,86 @@ using System.Threading.Tasks;
 
 namespace Animals
 {
+    enum AnimalSpecies { Dog, Panda, Rabit }
     class Animal
     {
+        string name;
+        int weight;
+        bool male;
+        AnimalSpecies spec;
 
+        public Animal(string name, int weight, bool male, AnimalSpecies spec)
+        {
+            this.name = name;
+            this.weight = weight;
+            this.male = male;
+            this.spec = spec;
+        }
+
+        public string Name { get { return name; } }
+        public int Weight { get { return weight; } }
+        public bool Male { get { return male; } }
+        public AnimalSpecies Spec { get { return spec; } }
     }
 
     class Cage
     {
+        Animal[] block;
+        int n;
 
+        public Cage(int size)
+        {
+            block = new Animal[size];
+            n = 0;
+        }
+
+        public void Add(Animal a)
+        {
+            if (n == block.Length)
+                return;
+            block[n++] = a;
+        }
+
+        public void Delete(string name)
+        {
+            int j = 0;
+
+            while (j < n && block[j].Name != name)
+                j++;
+
+            if(j<n)
+            {
+                for (int z = 0; z < n - 1; z++)
+                    block[z] = block[z+1];
+                n--;
+            }
+        }
+
+        public int CountSpecies(AnimalSpecies f)
+        {
+            int db = 0;
+            for (int i = 0; i < n; i++)
+                if (block[i].Spec == f)
+                    db++;
+            return db;
+        }
+
+        public bool SpeciesAndSexExisting(AnimalSpecies f, bool h)
+        {
+            int j = 0;
+            while (j < n && !(block[j].Spec == f && block[j].Male == h))
+                j++;
+            return j < n;
+        }
+
+        public Animal[] AnimalSpecies(AnimalSpecies f)
+        {
+            Animal[] t = new Animal[CountSpecies(f)];
+            int j = 0;
+            for (int i = 0; i < n; i++)
+                if (block[i].Spec == f)
+                    t[j++] = block[i];
+            return t;
+        }
     }
 }
--- 100_Classes/Animals/Animals/Program.cs
+++ 100_Classes/Animals/Animals/Program.cs
@@ -10,6 +10,18 @@ namespace Animals
     {
         static void Main(string[] args)
         {
+            Cage first = new Cage(5);
+            Cage second = new Cage(3);
+
+            first.Add(new Animal("Rex", 20, true, AnimalSpecies.Dog));
+            first.Add(new Animal("Mice", 320, false, AnimalSpecies.Panda));
+            first.Add(new Animal("Killer", 40, true, AnimalSpecies.Dog));
+            first.Add(new Animal("Roy", 30, true, AnimalSpecies.Dog));
+            first.Add(new Animal("Billy", 20, true, AnimalSpecies.Dog));
+
+            second.Add(new Animal("Molly", 4, true, AnimalSpecies.Rabit));
+            second.Add(new Animal("Puff", 320, false, AnimalSpecies.Panda));
+            second.Add(new Animal("Colly", 4, true, AnimalSpecies.Rabit));
         }
     }
 }

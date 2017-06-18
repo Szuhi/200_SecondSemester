0333efbd2d40d78e3fd57c305b37bf27ab48667e
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 15:31:13 +0200

    205_Exceptions Stacking, Final commit

---
+++ 205_Exceptions/Stacking/Stacking/Classes.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacking
+{
+    class Stack
+    {
+        int[] block;
+        int vm;
+
+        public Stack(int size)
+        {
+            block = new int[size];
+            vm = 0;
+        }
+
+        public void Push(int e)
+        {
+            if (vm == block.Length)
+                throw new StackIsFullException(e);
+            block[vm++] = e;
+        }
+
+        public int Pop()
+        {
+            if (vm == 0)
+                throw new StackIsEmptyException();
+            return block[--vm];
+        }
+    }
+
+    class StackIsFullException : Exception
+    {
+        int item;
+        public int Item { get { return item; } }
+        public StackIsFullException(int item)
+            :base ("Stack is full!")
+        {
+            this.item = item;
+        }
+    }
+
+    class StackIsEmptyException : Exception
+    {
+        public StackIsEmptyException()
+            :base ("Stack is empty!")
+        { }
+    }
+}
---
+++ 205_Exceptions/Stacking/Stacking/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stacking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Stack v = new Stack(5);
+                v.Push(1);
+                v.Push(2);
+                v.Push(3);
+                v.Push(4);
+                v.Push(5);
+
+                Console.WriteLine(v.Pop());
+                Console.WriteLine(v.Pop());
+                Console.WriteLine(v.Pop());
+                Console.WriteLine(v.Pop());
+                Console.WriteLine(v.Pop());
+                Console.WriteLine(v.Pop());
+            }
+            catch (StackIsFullException e)
+            {
+                Console.WriteLine("Couldn't store: " + e.Item);
+                Console.WriteLine(e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("It is going to be on the screen");
+            }
+
+            Console.ReadLine();
+        }
+    }
+}

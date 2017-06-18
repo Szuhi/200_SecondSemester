cf3acbb6c03773df881ca1837ad71de8508ac575
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 13:43:03 +0200

    102_Interfaces Pay, Final commit

---
+++ 102_Interfaces/Pay/Pay/Classes.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pay
+{
+    class PlasticCard : IOwnership
+    {
+        string owner;
+
+        public PlasticCard(string owner)
+        {
+            this.owner = owner;
+        }
+
+        public string Owner { get { return owner; } set { owner = value; } }
+    }
+    
+    class Card : PlasticCard, ICurrency
+    {
+        int account;
+
+        public Card(string owner, int account)
+            : base(owner)
+        {
+            this.account = account;
+        }
+
+        public virtual bool Paying(int sum)
+        {
+            if(account >= sum)
+            {
+                account -= sum;
+                return true;
+            }
+            return false;
+        }
+    }
+
+    class BannedCard : Card, ILost
+    {
+        string lost;
+
+        public BannedCard(string owner, int account, string lost)
+            : base(owner, account)
+        {
+            this.lost = lost;
+        }
+
+        public string Time { get { return lost; } }
+
+        public override bool Paying(int sum)
+        {
+            return false;
+        }
+    }
+
+    class Loan : ICurrency
+    {
+        int total;
+
+        public bool Paying(int sum)
+        {
+            total += sum;
+            return true;
+        }
+    }
+}
---
+++ 102_Interfaces/Pay/Pay/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pay
+{
+    interface ICurrency
+    {
+        bool Paying(int sum);
+    }
+    interface IOwnership
+    {
+        string Owner { get; set; }
+    }
+    interface ILost
+    {
+        string Time { get; }
+    }
+
+    class Program
+    {
+        static bool Payment(ICurrency[] t, int o)
+        {
+            foreach (ICurrency f in t)
+                if (!f.Paying(o))
+                    return false;
+            return true;
+        }
+
+        static bool ValidatePayment(ICurrency[] t, int o, string n)
+        {
+            foreach (ICurrency f in t)
+                if (!(f is IOwnership) || (f as IOwnership).Owner != n || !f.Paying(o))
+                    return false;
+            return true;
+        }
+
+        static void Main(string[] args)
+        {
+            ICurrency[] items = new ICurrency[]
+            {
+                new Card("Pure Pete", 1250),
+                new BannedCard("Wealthy Willy", 365000, "Yesterday"),
+                new Loan()
+            };
+
+            Console.WriteLine(Payment(items, 1000));
+            Console.WriteLine(ValidatePayment(items, 100, "Pure Pete"));
+
+            Console.ReadLine();
+        }
+    }
+}

aa09a3c1a751f4f312cda87b35505d0fc176d229
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 13:24:26 +0200

    102_Interfaces Small Example

---
+++ 102_Interfaces/SmallExample/SmallExample/Dog.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallExample
+{
+    class Dog : IComparable
+    {
+        string name;
+        int tailLength;
+
+        public Dog(string name, int tailLength)
+        {
+            this.name = name;
+            this.tailLength = tailLength;
+        }
+
+        public override string ToString()
+        {
+            return name + " (" + tailLength + " cm)";
+        }
+
+        public int CompareTo(object obj)
+        {
+            return tailLength - (obj as Dog).tailLength;
+        }
+    }
+}
---
+++ 102_Interfaces/SmallExample/SmallExample/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmallExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog[] cage = new Dog[]
+            {
+                new Dog("Alfons", 7),
+                new Dog("Berni", 14),
+                new Dog("Ceasar", 6),
+                new Dog("Dome", 4),
+                new Dog("Einstein", 12),
+            };
+
+            Write(cage);
+            Array.Sort(cage);
+
+            Write(cage);
+            Console.ReadLine();
+        }
+
+        static void Write(Dog[] t)
+        {
+            foreach (Dog d in t)
+                Console.WriteLine(d);
+            Console.WriteLine();
+        }
+    }
+}

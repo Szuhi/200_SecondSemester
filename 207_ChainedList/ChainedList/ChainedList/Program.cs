fae3753ee4e263e23fa301b7d3324ea80dcdc7aa
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 17:29:20 +0200

    207_ChainedList Final commit

--- 207_ChainedList/ChainedList/ChainedList/Program.cs
+++ 207_ChainedList/ChainedList/ChainedList/Program.cs
@@ -10,11 +10,48 @@ namespace ChainedList
     {
         static void Main(string[] args)
         {
+            ChainedList<int> list = new ChainedList<int>();
+            list.InsertAtTheBegining(4);
+            list.InsertAtTheBegining(3);
+            list.InsertAtTheBegining(5);
+            list.Listing();
+
+            ChainedList<string> list2 = new ChainedList<string>();
+            list2.InsertAtTheBegining("Thursday.");
+            list2.InsertAtTheBegining("Is");
+            list2.InsertAtTheBegining("It");
+            list2.Listing();
+
+            Console.ReadLine();
         }
     }
 
-    class ChainedList
+    class ChainedList<T>
     {
+        class ListItem
+        {
+            public T content;
+            public ListItem next;
+        }
 
+        ListItem head = null;
+
+        public void InsertAtTheBegining(T item)
+        {
+            ListItem newItem = new ListItem();
+            newItem.content = item;
+            newItem.next = head;
+            head = newItem;
+        }
+
+        public void Listing()
+        {
+            ListItem actual = head;
+            while (actual != null)
+            {
+                Console.WriteLine(actual.content);
+                actual = actual.next;
+            }
+        }
     }
 }

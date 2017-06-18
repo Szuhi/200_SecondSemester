cd418da8ceb2229cc72be2c9ba64e44f4accab80
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 14:32:13 +0200

    204_Events Calling, Final commit

---
+++ 204_Events/Calling/Calling/Classes.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling
+{
+    class Telephone
+    {
+        string number;
+        int account;
+        ICallObserver observer;
+        const int CHARGE = 200;
+
+        public Telephone(string number)
+        {
+            this.number = number;
+        }
+
+        public void UploadAccount(int sum)
+        {
+            account += sum;
+        }
+
+        public void ObserveRegister(ICallObserver f)
+        {
+            observer = f;
+        }
+
+        void ReceiveCall(Telephone source)
+        {
+            if (observer != null)
+                observer.IncomingCall(this, source.number);
+        }
+
+        public void Calling(Telephone dest)
+        {
+            if (observer != null)
+                observer.OutgoingCall(this, dest.number);
+            if(account >= CHARGE)
+            {
+                dest.ReceiveCall(this);
+                account -= CHARGE;
+            }
+        }
+
+        public override string ToString()
+        {
+            return number;
+        }
+    }
+
+    class CallJournal : ICallObserver
+    {
+        public void IncomingCall(Telephone sender, string source_number)
+        {
+            Console.WriteLine("Incoming call: " + sender + ", source: " + source_number);
+        }
+
+        public void OutgoingCall(Telephone sender, string dest_number)
+        {
+            Console.WriteLine("Outgoing call: " + sender + ", destination: " + dest_number);
+        }
+    }
+
+    class FileJournal : ICallObserver
+    {
+        public void IncomingCall(Telephone sender, string source_number)
+        {
+            System.IO.File.AppendAllText("journal.log", "Incoming call: " + sender + ", source: " + source_number + "\r\n");
+        }
+
+        public void OutgoingCall(Telephone sender, string dest_number)
+        {
+            System.IO.File.AppendAllText("journal.log", "Outgoing call: " + sender + ", source: " + dest_number + "\r\n");
+        }
+    }
+}
---
+++ 204_Events/Calling/Calling/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calling
+{
+    interface ICallObserver
+    {
+        void IncomingCall(Telephone sender, string source_number);
+        void OutgoingCall(Telephone sender, string dest_number);
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Telephone t1 = new Telephone("1"), t2 = new Telephone("2");
+            CallJournal n = new CallJournal();
+            t1.ObserveRegister(n);
+            t2.ObserveRegister(new FileJournal());
+            t1.UploadAccount(500);
+            t1.Calling(t2);
+            t1.Calling(t2);
+            t2.Calling(t1);
+            t2.Calling(t1);
+            t1.Calling(t2);
+            t1.Calling(t2);
+            t2.Calling(t1);
+
+            Console.ReadLine();
+        }
+    }
+}

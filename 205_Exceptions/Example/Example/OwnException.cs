3f0262693b91eed4e25e6d084f89392ff922fbb1
Szuhi <szuhanszky.tamas@gmail.com>
2017-06-18 15:22:07 +0200

    205_Exceptions Example

---
+++ 205_Exceptions/Example/Example/OwnException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class OwnException : Exception
+    {
+        int number;
+        public int Number { get { return number; } }
+
+        public OwnException(int number)
+            : base("Own exception happened!")
+        {
+            this.number = number;
+        }
+    }
+}
---
+++ 205_Exceptions/Example/Example/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                int.Parse(Console.ReadLine());
+                int d = 0;
+                int[] t = new int[] { 1, 2, 3 };
+                throw new OwnException(42);
+            }
+            catch (FormatException e)
+            {
+                Console.WriteLine("It's not a number!");
+            }
+            catch (OwnException e)
+            {
+                Console.WriteLine("The stored value is: " + e.Number);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
